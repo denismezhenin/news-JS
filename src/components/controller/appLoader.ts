d4e7f3682782c37e96cd37de65ce7609f0c84242
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2022-12-12 22:37:23 +0300

    fix: change proxy server

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '78f36bf61fa84b27a488b49d2fb4407c', // получите свой ключ https://newsapi.org/
         });
     }
